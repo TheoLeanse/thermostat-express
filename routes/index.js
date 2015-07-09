cf95d454b0960661f9030f54f8ecefe359654865
TheoLeanse <theoleanse@gmail.com>
2015-07-09 07:28:26 +0100

    playing with routes in Express.js

--- routes/index.js
+++ routes/index.js
@@ -6,4 +6,8 @@ router.get('/', function(req, res, next) {
   res.render('index', { title: 'Thermostat' });
 });
 
+router.get('/test', function(req, res) {
+  res.render('test', { title: 'Testing out' });
+});
+
 module.exports = router;
